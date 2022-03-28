c95a27093382166e6d98c91b3e310b995cfd65e7
crabbit-git <98990717+crabbit-git@users.noreply.github.com>
2022-03-28 10:47:46 +0100

    3 basic tests written

---
+++ tests/fizzbuzz_test.py
@@ -0,0 +1,14 @@
+import unittest
+
+from src.fizzbuzz import *
+
+class TestFizzBuzz(unittest.TestCase):
+    
+    def test_fizzbuzz__3_returns_fizz(self):
+        self.assertEqual("Fizz", fizzbuzz(3))
+    
+    def test_fizzbuzz__5_returns_buzz(self):
+        self.assertEqual("Buzz", fizzbuzz(5))
+    
+    def test_fizzbuzz__15_returns_fizzbuzz(self):
+        self.assertEqual("FizzBuzz", fizzbuzz(15))
